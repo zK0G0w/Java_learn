fd660223b234f21501ef62590be1a8f1be6cc0c4
WainZeng <zk0g0w@gmail.com>
2024-07-02 21:41:46 +0800

    nothing

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-public class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("Hello,world");
-=======
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello,World");
->>>>>>> 120d5e0c843abfa316012e3925c44c1b297991ee
+        System.out.println("7" + 2);
     }
 }
