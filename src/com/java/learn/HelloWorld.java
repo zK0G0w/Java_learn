adf86aef6d7becc3e44d33f9fbc86bd255840cc1
WainZeng <zk0g0w@gmail.com>
2024-08-13 15:42:39 +0800

    Hellor,World!

--- src/com/java/learn/HelloWorld.java
+++ src/com/java/learn/HelloWorld.java
@@ -1,6 +1,7 @@
+package com.java.learn;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("7" + 2);
+        System.out.println("Hello,World!");
     }
 }
